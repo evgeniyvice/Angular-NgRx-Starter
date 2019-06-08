67b2cb4e1bee0deac13512a8b676a7c0649ef1b9
Vice Evgen <vice.evgen@yandex.ru>
2019-06-08 12:21:37 +0300

    add ngrx libs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule } from '@angular/router'
+import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store'
+import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { environment } from '../environments/environment'
 
 import { MaterialModule } from './material/material.module'
 
@@ -13,6 +18,19 @@ import { AppComponent } from './app.component'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    StoreModule.forRoot({
+      router: routerReducer,
+    }),
+    // Instrumentation must be imported after importing StoreModule (config is optional)
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+    RouterModule.forRoot([
+      // routes
+    ]),
+    // Connects RouterModule with StoreModule
+    StoreRouterConnectingModule.forRoot(),
     MaterialModule
   ],
   providers: [],
